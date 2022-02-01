aedad12984f2228f477cd986b32f8fa39f77ee3a
ciniminisman <98621148+ciniminisman@users.noreply.github.com>
2022-02-01 21:34:45 +0100

    about me

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,38 +5,19 @@ module.exports = {
     // Your Name
     name: 'Bagrat Rukhadze',
     // Main Site Title
-    title: `Bagrat Rukhadze | Quality Assurance Manager`,
+    title: `Bagrat Rukhadze | QA Manager`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Quality Assurance Manager`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/ciniminisman`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/bagratr/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `Software quality assurance / test manager in web, native and mobile with 2+ years of hands-on testing experience.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
-    projects: [
-      {
-        name: 'Devfolio',
-        description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'ChromeExtensionKit',
-        description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://chromeextensionkit.com/?ref=devfolio',
-      },
-      {
-        name: 'Another Cool Project',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-    ],
+   
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
